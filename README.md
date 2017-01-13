4fa851b1da4bf01170dfba8a2ca8419653c007b8
hamzeen.hameem <Hamzeen.Hameem@sc.com>
2017-01-13 09:22:46 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# angular2-superfast
+# a super fast Angular2 seed #
+
+## Table of Contents ##
+
+- [Setup & Build](##setup-&-build)
+- [License](##license)
+
+## setup & build ##
+
+```bash
+npm i
+npm run build_prod
+npm run serve
+```
+
+Now open your browser at http://localhost:5557
+
+>created on Friday, 13th Jan 2017
+
+## LICENSE ##
+Copyright © 2017 Hamzeen. H.
+Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this project. It includes the rights to use,
+fork, modify, merge, publish and distribute.
+
+THE SOURCE OF THIS PROJECT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
---
+++ dist/app.component.ngfactory.js
@@ -0,0 +1,76 @@
+import * as import0 from './app.component';
+import * as import1 from '@angular/core/src/linker/view';
+import * as import3 from '@angular/core/src/linker/view_utils';
+import * as import4 from '@angular/core/src/metadata/view';
+import * as import5 from '@angular/core/src/linker/view_type';
+import * as import6 from '@angular/core/src/change_detection/constants';
+import * as import7 from '@angular/core/src/linker/component_factory';
+export class Wrapper_AppComponent {
+    constructor() {
+        this._changed = false;
+        this.context = new import0.AppComponent();
+    }
+    ngOnDetach(view, componentView, el) {
+    }
+    ngOnDestroy() {
+    }
+    ngDoCheck(view, el, throwOnChange) {
+        var changed = this._changed;
+        this._changed = false;
+        return changed;
+    }
+    checkHost(view, componentView, el, throwOnChange) {
+    }
+    handleEvent(eventName, $event) {
+        var result = true;
+        return result;
+    }
+    subscribe(view, _eventHandler) {
+        this._eventHandler = _eventHandler;
+    }
+}
+var renderType_AppComponent_Host = import3.createRenderComponentType('', 0, import4.ViewEncapsulation.None, [], {});
+class View_AppComponent_Host0 extends import1.AppView {
+    constructor(viewUtils, parentView, parentIndex, parentElement) {
+        super(View_AppComponent_Host0, renderType_AppComponent_Host, import5.ViewType.HOST, viewUtils, parentView, parentIndex, parentElement, import6.ChangeDetectorStatus.CheckAlways);
+    }
+    createInternal(rootSelector) {
+        this._el_0 = import3.selectOrCreateRenderHostElement(this.renderer, 'my-app', import3.EMPTY_INLINE_ARRAY, rootSelector, null);
+        this.compView_0 = new View_AppComponent0(this.viewUtils, this, 0, this._el_0);
+        this._AppComponent_0_3 = new Wrapper_AppComponent();
+        this.compView_0.create(this._AppComponent_0_3.context);
+        this.init(this._el_0, (this.renderer.directRenderer ? null : [this._el_0]), null);
+        return new import7.ComponentRef_(0, this, this._el_0, this._AppComponent_0_3.context);
+    }
+    injectorGetInternal(token, requestNodeIndex, notFoundResult) {
+        if (((token === import0.AppComponent) && (0 === requestNodeIndex))) {
+            return this._AppComponent_0_3.context;
+        }
+        return notFoundResult;
+    }
+    detectChangesInternal(throwOnChange) {
+        this._AppComponent_0_3.ngDoCheck(this, this._el_0, throwOnChange);
+        this.compView_0.internalDetectChanges(throwOnChange);
+    }
+    destroyInternal() {
+        this.compView_0.destroy();
+    }
+    visitRootNodesInternal(cb, ctx) {
+        cb(this._el_0, ctx);
+    }
+}
+export const AppComponentNgFactory = new import7.ComponentFactory('my-app', View_AppComponent_Host0, import0.AppComponent);
+const styles_AppComponent = [];
+var renderType_AppComponent = import3.createRenderComponentType('', 0, import4.ViewEncapsulation.None, styles_AppComponent, {});
+export class View_AppComponent0 extends import1.AppView {
+    constructor(viewUtils, parentView, parentIndex, parentElement) {
+        super(View_AppComponent0, renderType_AppComponent, import5.ViewType.COMPONENT, viewUtils, parentView, parentIndex, parentElement, import6.ChangeDetectorStatus.CheckAlways);
+    }
+    createInternal(rootSelector) {
+        const parentRenderNode = this.renderer.createViewRoot(this.parentElement);
+        this._text_0 = this.renderer.createText(parentRenderNode, 'Hello world!', null);
+        this.init(null, (this.renderer.directRenderer ? null : [this._text_0]), null);
+        return null;
+    }
+}
+//# sourceMappingURL=app.component.ngfactory.js.map
